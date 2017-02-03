e28495596117cdb28fab0ca671bc91a1d30ab032
giulianna <j.bustinza42@gmail.com>
2017-02-03 11:49:12 -0500

    falta

--- main.js
+++ main.js
@@ -1,41 +1,47 @@
-var listaTareas = [];
+//var listaTareas = [];
 var tareaInput=document.getElementById("tareaInput");
 var btnNuevaTarea=document.getElementById("btn-agregar");
-
-//var listaTareas = [{nombre:"tarea1",isDone:true},{nombre:"tarea2",isDone:false}];
+var listaTareas = [{nombre:tareaInput,isDone:false}];
 
 
 //funcion k encarga  de cargar las tareas en lista
 function drawTasksList(){
    var lista=document.getElementById("lista"); 
     
-    
     for(var i in listaTareas) {
-        var html = "<li><input type='checkbox' "+(listaTareas[i].isDone?"checked ":"")+">"+listaTareas[i].nombre + "<i type='onclick'  class='glyphicon  glyphicon-trash , icon , pull-right '>" +"</li>";
+        var html = "<li><input onclick='select'(+i+)type='checkbox' "+(listaTareas[i].isDone?"checked ":"")+">"+listaTareas[i].nombre.value + "<i type='onclick'  class='glyphicon  glyphicon-trash , icon , pull-right '>" +"</li>";
         
         lista.innerHTML+=html;
-    }    
+    }     
 }
-// Agregar Tarea
-	btnNuevaTarea.addEventListener("click", agregarTarea);
 
-var agregarTarea = function(){
-
-		var tarea = tareaInput.value;
-		var	contenido = document.createTextNode(tarea);
-        
-        		if (tarea === "") {
-			tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
-			tareaInput.className = "error";
-			return false;
-		}
-    lista.appendChild(nuevaTarea)
-    
-
-
-
-
-
+/*---------------------------------------------------------------*/
+// funcion para eliminar la tarea haciendo click al icono:
+/*//  funcion principal que crea los elemnetos en el html:
+function drawTextList(){
+    var input = document.getElementById("mensaje");
+    var valorInput = input.value;
+// nuevo metodo:
+    var listaTareas = [ {nombre: valorInput, isDone:false} ];
+    var lista = document.getElementById("lista");
+    for(var i in listaTareas){
+        var html = "<li> <input onclick='tachar' type = 'checkbox' " + (listaTareas[i].isDone?"checked":"") +  ">" + 
+        listaTareas[i].nombre + "<span class='glyphicon  glyphicon-trash , icon , pull-right'>" + "</span>" +"</li>";
+        lista.innerHTML += html;
+    }
+    var valorInput = document.getElementById("mensaje").value="";
+    eliminarTarea();
+}
+// funcion para eliminar la tarea haciendo click al icono:
+function eliminarTarea(){
+    var eliminar = document.getElementsByClassName("icon");
+    for(var c = 0; c < eliminar.length; c++){
+        eliminar[c].onclick = function(){
+        var lista = this.parentElement;
+        lista.style.display = "none";
+       }
+    }
+};*/
 
 
 
