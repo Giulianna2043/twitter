b1ce2467af4db07fb41d29a725b8f0d96ac87b9c
giulianna <j.bustinza42@gmail.com>
2017-02-06 16:11:03 -0500

    AVANCE

--- main.js
+++ main.js
@@ -4,21 +4,6 @@ var tareaInput=document.getElementById("tareaInput");
 
 //var btnNuevaTarea=document.getElementById("btn-agregar");
 //btn-agregar.setAttribute("onclick","onButtonClick()");[
-function onMensajeKey(evt)
-{
-    console.log();
-  if(evt.keyCode == 13)
-  {
-    var tareaInput=document.getElementById("tareaInput");
-    agregar();
-
-    elInputMensajes.value="";
-  }
-}}
-
-
-
-
 function agregar() {
 	if(tareaInput.value=="" || tareaInput.value==0){
 		tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
@@ -41,6 +26,67 @@ function drawTasksList(){
 		tareaInput.value="";
 }
 
+/*---------------------------------------------------------------*/
+/*
+var textarea = document.getElementById("texto");
+
+
+var listaTareas = [];
+
+
+function agregar(){
+
+	if(textarea.value == "" || textarea.value == 0){
+		alert('Tienes que ingresar tarea');
+	}else{
+
+		listaTareas.push({nombre:textarea.value, isDone:false});
+		drawTasksList();
+	}
+}
+
+
+function drawTasksList(){
+
+	var lista = document.getElementById("lista");
+
+	//funcion dibujar tareas
+	lista.innerHTML = "";
+	for (var i in listaTareas){
+		var html = "<li class='works' style = '"+ (listaTareas[i].isDone?"text-decoration:line-through":"") + "' ><input type='checkbox' onclick='checkList("+i+")'  "+ (listaTareas[i].isDone?"checked":"") + ">" + listaTareas[i].nombre + "<i class='glyphicon glyphicon-trash' onclick='selecTach("+i+")'>" + "</Li>";
+
+		lista.innerHTML += html;
+
+	}
+
+	textarea.value = "";
+	textarea.focus();
+	}
+
+
+
+function checkList(box){
+
+	listaTareas[box].isDone = !listaTareas[box].isDone;
+
+
+	/*if(listaTareas[box].isDone == false){
+		listaTareas[box].isDone = true;
+		//console.log();
+		//lista.getElementsByClassName("works")[box].style.textDecoration="line-through";
+	}else{
+		listaTareas[box].isDone = false;
+		//lista.getElementsByClassName("works")[box].style.textDecoration="none";
+	}*/
+
+	/*();
+}
+
+function selecTach(tach){
+	listaTareas.splice(tach,1);
+	drawTasksList();
+
+}*/
 
 
 
